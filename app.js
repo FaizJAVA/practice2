4c23210b8b2d5656254da7694a0131e401a76ac0
FaizJAVA <rockrad0007@gmail.com>
2022-04-02 15:41:26 +0530

    newdata

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express=require('express');
 const bodyparser=require('body-parser');
 const mongoose=require('mongoose');
 const userImport=require('./route/userroute');
+const cors=require('cors');
 
 mongoose.connect('mongodb+srv://Faizaan:123@pract2cluster.6su1g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(()=>{
     console.log('Success');
@@ -14,6 +15,7 @@ const port=process.env.PORT||3000;
 const app=express();
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
+app.use(cors());
 app.use('/api/user/',userImport);
 
 app.listen(port,()=>{
